9d52971e59c3bdd1c64c4a01ad700fac7558bb52
Fulloa08 <f.ulloa08@ufromail.cl>
2025-04-10 10:57:56 -0400

    5to commit último código actualizado

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@
 import java.util.Scanner;
 import java.util.Random;
 
-
-public static void Main(String[] args){
+public class TallerUno {
+    static int[][] matriz;
     static int filas;
     static int columnas;
-    static int[][] matriz;
-    Scanner scanner = new Scanner(System.in);
-    dimensionMatriz(scanner);
-    menu();
-    leerOpcion();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ejecutarMenu(scanner);
 
     }
-    public static void menu(){
+
+    public static void mostrarMenu() {
         System.out.println("-------- MENU PRINCIPAL ------------");
         System.out.println("1. Ingresar dimensiones matriz");
         System.out.println("2. Buscar fila de la matriz");
@@ -22,66 +22,74 @@ public static void Main(String[] args){
         System.out.println("4. Salir");
         System.out.println("Ingrese la opción:  ");
     }
-    public static void leerOpcion(int[][] matriz){
-        Scanner scanner = new Scanner(System.in);
-        int opcion = 0;
-        opcion = scanner.nextInt();
+    public static void ejecutarMenu(Scanner scanner){
+        int opcion;
         do {
-            menu();
+            mostrarMenu();
+            opcion = scanner.nextInt();
+            procesarOpcion(scanner, opcion);
+        } while (opcion != 4);
+        System.out.println("EL PROGRAMA HA FINALIZADO. ");
+    }
 
-            switch (opcion) {
-                case 1:
-                    dimensionMatriz(scanner);
-                    break;
-                case 2:
-                    System.out.println("Ingrese la fila a buscar:  ");
-                    String busqueda = scanner.next();
-                    mostrarFila(busqueda, matriz);
-                    break;
-                case 3:
-                    matrizCero();
-                    break;
-                default:
-                    System.out.println("Opción inválida");
-            }
-        }while  (opcion != 4);
-        System.out.println("EL PROGRAMA HA FINALIZADO");
+    public static void procesarOpcion(Scanner scanner, int opcion){
+        switch (opcion){
+            case 1 -> dimensionMatriz(scanner);
+            case 2 -> mostrarFila(scanner);
+            case 3 -> matrizCero();
+            case 4 -> {}
+            default -> System.out.println("Opción inválida.");
 
+        }
     }
 
 
-    public int[][] crearMatriz(int filas, int columnas){
+    public int[][] crearMatriz(int filas, int columnas) {
         return new int[filas][columnas];
 
     }
-    public static void llenarMatriz(int[][] matriz){
+
+    public static void llenarMatriz(int[][] matriz) {
         Random random = new Random();
-        for (int i = 0; i <matriz.length; i++){
-            for (int j = 0; j <matriz.length; j++){
-                matriz[i][j] = (int) (Math.random()*10);
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = random.nextInt(10);
             }
         }
 
     }
-    public boolean matrizCero (int[][] matriz){
+
+    public boolean matrizCero(int[][] matriz) {
+        int total = matriz.length * matriz[0]. length;
+        int ceros = 0;
 
 
     }
-    public static void dimensionMatriz(Scanner scanner){
+
+    public static void dimensionMatriz(Scanner scanner) {
         System.out.println("Ingrese la dimensión de la fila: ");
-        int filas = scanner.nextInt();
+        filas = scanner.nextInt();
 
         System.out.println("Ingrese la dimensión de la columna: ");
-        int columnas = scanner.nextInt();
+        columnas = scanner.nextInt();
+
+        if(validarDimensiones(filas, columnas)){
+            matriz = crearMatriz(filas, columnas);
+            llenarMatriz(matriz);
+            System.out.println("Matriz creada y llenada con valores aleatorios.");
+        } else {
+            System.out.println("Dimensiones inválidas");
+        }
+
+    }
+
+    public static int[] mostrarFila(String busqueda, int[][] matriz) {
 
 
     }
-    public static int[] mostrarFila(String busqueda, int[][] matriz){
 
-
+    public static boolean validarDimensiones(int fls, int cols) {
+        return fls > 1 && cols > 1;
     }
-    public static boolean validarDimensiones(int fls, int cols){
-        return fls > 1 && cols >1;
-    }
-
+}
 
